6fd155e47c33d28286c3f08855ec8fc7553e90bc
iamsainikhil <sainikhil.bheema@gmail.com>
2020-05-14 19:50:35 -0400

    test app deployment on vercel

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,4 @@
  * weather react serverless API functions URL
  */
 const isProduction = process.env.NODE_ENV === 'production'
-export default isProduction
-  ? 'https://weather-react-api.now.sh'
-  : 'https://weather-react-api-dev.now.sh'
+export default isProduction ? 'api' : 'https://weather-react-api-dev.now.sh'
