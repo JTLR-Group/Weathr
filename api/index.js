fa89e65c6155cb8aee8c7a72ab2b75c45cfa197c
iamsainikhil <sainikhil.bheema@gmail.com>
2020-05-14 22:52:10 -0400

    update api endpoints

--- api/index.js
+++ api/index.js
@@ -1,8 +1,73 @@
+const fetchAddressInfo = (latlong) => {
+  const url = `https://places-dsn.algolia.net/1/places/reverse?aroundLatLng=${latlong},&hitsPerPage=1&language=en`
+  axios
+    .get(url, {headers: HEADERS})
+    .then((response) => {
+      const {data} = response
+      res.status(200)
+      res.json(data)
+    })
+    .catch((err) => {
+      res.status(err.response ? err.response.status : 500)
+      res.send(err.message || 'Something went wrong! Please try again later.')
+    })
+}
+
+const fetchCities(req) => {
+  const {city, latlong} = req.query
+  axios
+    .request({
+      url: 'https://places-dsn.algolia.net/1/places/query',
+      method: 'post',
+      data: {
+        query: city,
+        type: 'city',
+        aroundLatLng: latlong,
+      },
+      headers: HEADERS,
+    })
+    .then((response) => {
+      const {data} = response
+      res.status(200)
+      res.json(data)
+    })
+    .catch((err) => {
+      res.status(err.response ? err.response.status : 500)
+      res.send(err.message || 'Something went wrong! Please try again later.')
+    })
+}
+
+const fetchForecast(latlong) {
+  const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${latlong}?extend=hourly&exclude=minutely,flags`
+  axios
+    .get(url)
+    .then((response) => {
+      const {data} = response
+      res.status(200)
+      res.json(data)
+    })
+    .catch((err) => {
+      res.status(err.response ? err.response.status : 500)
+      res.send(err.message || 'Something went wrong! Please try again later.')
+    })
+}
+
+
 module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    params: req.params,
-    cookies: req.cookies,
-  })
+  const {type, latlong} = req.query
+
+  switch(type) {
+    case 'address':
+      fetchAddressInfo(latlong);
+      break;
+    case 'places':
+      fetchCities(req);
+      break;
+    case 'forecast':
+      fetchForecast(latlong);
+      break;
+    default:
+      res.send('Welcome to Weather React API');
+      break;
+  }
 }
