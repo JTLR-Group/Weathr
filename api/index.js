56bc16012d815b4ce3f2c59db77712ab468c68d8
iamsainikhil <sainikhil.bheema@gmail.com>
2020-05-14 21:50:01 -0400

    play with api endpoint

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
 module.exports = (req, res) => {
-  res.status(200).send(`request is ${req.json()}`)
+  res
+    .status(200)
+    .send(`request is ${req.query} ${req.params ? req.params : ''}`)
 }
