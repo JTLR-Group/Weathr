1480b5d19c8c307a358df4e395d4e832d3fc93a3
iamsainikhil <sainikhil.bheema@gmail.com>
2020-05-14 23:18:41 -0400

    update api functions

--- api/index.js
+++ api/index.js
@@ -55,19 +55,57 @@ const fetchForecast = (latlong) => {
 module.exports = (req, res) => {
   const {type, latlong} = req.query
 
-  fetchAddressInfo(latlong, res)
-  // switch (type) {
-  //   case 'address':
-  //     fetchAddressInfo(latlong)
-  //     break
-  //   case 'places':
-  //     fetchCities(req)
-  //     break
-  //   case 'forecast':
-  //     fetchForecast(latlong)
-  //     break
-  //   default:
-  //     res.send('Welcome to Weather React API')
-  //     break
-  // }
+  fetchAddressInfo: () => {
+    const url = `https://places-dsn.algolia.net/1/places/reverse?aroundLatLng=${latlong},&hitsPerPage=1&language=en`
+    axios
+      .get(url, {headers: HEADERS})
+      .then((response) => {
+        const {data} = response
+        res.status(200)
+        res.json(data)
+      })
+      .catch((err) => {
+        res.status(err.response ? err.response.status : 500)
+        res.send(err.message || 'Something went wrong! Please try again later.')
+      })
+  }
+
+  fetchCities: () => {
+    const {city, latlong} = req.query
+    axios
+      .request({
+        url: 'https://places-dsn.algolia.net/1/places/query',
+        method: 'post',
+        data: {
+          query: city,
+          type: 'city',
+          aroundLatLng: latlong,
+        },
+        headers: HEADERS,
+      })
+      .then((response) => {
+        const {data} = response
+        res.status(200)
+        res.json(data)
+      })
+      .catch((err) => {
+        res.status(err.response ? err.response.status : 500)
+        res.send(err.message || 'Something went wrong! Please try again later.')
+      })
+  }
+
+  switch (type) {
+    case 'address':
+      fetchAddressInfo()
+      break
+    case 'places':
+      fetchCities()
+      break
+    case 'forecast':
+      fetchForecast()
+      break
+    default:
+      res.send('Welcome to Weather React API')
+      break
+  }
 }
